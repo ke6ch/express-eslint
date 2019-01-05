351ec0118990b498863ac185d9b21fd608875917
k <k@example.com>
2019-01-06 00:56:17 +0900

    Add React files(Form,List) & Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import React from 'react'
+import List from './List'
+import Form from './Form'
 
 export default class App extends React.Component {
+  state = {
+    tasks: []
+  };
+
+  handleSubmit = task => {
+    this.setState({ tasks: [...this.state.tasks, task] });
+  }
+
+//  handleSearch = () => {
+  componentDidMount = () => {
+    let url = 'http://localhost:3000/api/v1/task';
+
+    fetch(url).then(response => {
+      return response.json();
+    }).then(data => {
+      this.setState({ tasks: data });
+    });
+  }
+
   render () {
-    return <h1>Hello, world!</h1>
+    const { tasks } = this.state;
+
+    return (
+      <div>
+        <List tasks={tasks} />
+        <Form handleSubmit={this.handleSubmit}/>
+      </div>
+    );
   }
 }
