8b35b5f4a231afecf6fb0453237eefbfe6582ab6
ke6ch <19749261+ke6ch@users.noreply.github.com>
2019-01-06 00:13:42 +0900

    Add mongoose, model

--- app.js
+++ app.js
@@ -2,12 +2,26 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var tasksRouter = require('./routes/tasks');
 var apiRouter = require('./routes/api');
+
+// setup mongoose connection
+var mongoDB = 'mongodb://localhost/mongo';
+
+mongoose.connect(mongoDB)
+  .then(() =>  console.log('connection succesful'))
+  .catch((err) => console.error(err));
+mongoose.Promise = global.Promise;
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 var app = express();
 
@@ -19,6 +33,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
