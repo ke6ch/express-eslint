8dda67ad30748dee48fd9dabbfe4c528b50729c9
k <k@example.com>
2019-01-06 00:05:41 +0900

    Add index,store method

--- app/controllers/apicontroller.js
+++ app/controllers/apicontroller.js
@@ -1,3 +1,11 @@
-exports.index = function (req, res) {
-  res.send('NOT IMPLEMENTED: Site Home Page');
+const Task = require('../models/task');
+
+exports.index = (req, res) => {
+  Task.find({}, null, { sort: { date: 1 } }, (err, data) => {
+    res.json(data);
+  });
+};
+
+exports.store = (req, res) => {
+  res.send('store');
 };
