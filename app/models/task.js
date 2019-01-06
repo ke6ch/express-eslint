bf4cd9018297c2127e6881c7d1725f786b934029
k <k@example.com>
2019-01-07 00:01:12 +0900

    Update date default

--- app/models/task.js
+++ app/models/task.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const Task = new Schema({
   no: { type: Number, require: true, unique: true },
   task: { type: String, require: true },
-  date: { type: Date, default: Date(9999-12-31) },
+  date: { type: Date, default: '12/31/9999' },
   status: { type: Boolean, require: true, default: true },
 });
 
