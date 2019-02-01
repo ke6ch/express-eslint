82dd5202c861d36171cec085e0bcc81312298198
k <k@example.com>
2019-02-01 22:42:21 +0900

    Update log4js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var logger = require('morgan');
+var log4js = require('./config/log4js');
 var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
@@ -29,7 +29,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
