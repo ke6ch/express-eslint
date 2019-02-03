fa9c5f4aff758a163fcd932734eee795b45c41fc
k <k@example.com>
2019-02-04 00:42:20 +0900

    Update date field check

--- routes/api.js
+++ routes/api.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get('/task', apicontroller.index);
 router.post('/task', apicontroller.store);
+//router.post('/task', apicontroller.validate, apicontroller.store);
 
 module.exports = router;
