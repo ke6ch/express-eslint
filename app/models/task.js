729529945eba0573ff49bdb83c7ddddaa9438103
k <k@example.com>
2019-02-04 00:40:27 +0900

    Delete No field

--- app/models/task.js
+++ app/models/task.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Task = new Schema({
-  no: { type: Number, require: true, unique: true },
   task: { type: String, require: true },
   date: { type: Date, default: '12/31/9999' },
   status: { type: Boolean, require: true, default: true },
