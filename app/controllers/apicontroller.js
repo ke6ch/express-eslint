43219006a63caf29c359bd47fefc56f8f3ef5d72
ke6ch <19749261+ke6ch@users.noreply.github.com>
2019-02-13 23:01:18 +0900

    Update validation

--- app/controllers/apicontroller.js
+++ app/controllers/apicontroller.js
@@ -2,20 +2,29 @@ const { check, validationResult } = require('express-validator/check');
 const Task = require('../models/task');
 
 // Validate
-exports.validate = (req, res) => {
-  check('task', 'task is required').not().exists();
+exports.validate = () => {
+  return [
+    check('task', 'task is required').not().exists(),
+  ];
 };
 
 // GET task
 exports.index = (req, res) => {
   Task.find({}, null, { sort: { date: 1 } }, (err, data) => {
+    console.log(data);
     res.json(data);
   });
 };
 
 // POST task
 exports.store = (req, res) => {
-  // TODO: Add Validate Check
+  // Error Handling
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    //return res.status(422).json({ errors: errors.array() });
+    return res.status(404).json({ errors: errors(msg) });
+  }
+
   let reqDate;
 
   if (req.body.date !== '') {
